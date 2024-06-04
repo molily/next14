e375938b2f0235d127a6a6a3f8f13fe0ff0c1523
Mathias Schaefer (molily) <molily@mailbox.org>
2024-06-04 11:31:41 +0200

    Test aliases

--- next-dir/next.config.mjs
+++ next-dir/next.config.mjs
@@ -1,27 +1,42 @@
 import * as path from "node:path";
 
+import { fileURLToPath } from "node:url";
+
+const __dirname =
+  import.meta.dirname || path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config, context) => {
-    console.log('config.module.rules:',config.module.rules);
+    console.log("config.module.rules:", config.module.rules);
 
     const mappedRules = config.module.rules.map((rule) => {
       // Disable next-image-loader for SVG files
       if (rule.loader === "next-image-loader") {
         // console.log("MUTATE RULE", rule);
-        return { ...rule,
+        return {
+          ...rule,
           // Remove .svg from RegExp
-          test: /\.(png|jpg|jpeg|gif|webp|avif|ico|bmp)$/i };
+          test: /\.(png|jpg|jpeg|gif|webp|avif|ico|bmp)$/i,
+        };
       }
       return rule;
     });
 
-    const svgPath = path.resolve(import.meta.dirname, "../components");
+    const svgPath = path.resolve(__dirname, "../components");
     console.log("svgPath", svgPath);
 
     return {
       ...config,
+
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          "@components": path.resolve(__dirname, "../components"),
+        },
+      },
 
       module: {
         rules: [
@@ -41,6 +56,12 @@ const nextConfig = {
               if (matched) console.log("MATCH", item);
               return matched;
             },
+            type: "asset/resource",
+          },
+
+          {
+            resourceQuery: /url/,
+            test: /.txt$/,
             type: "asset/resource",
           },
         ],
--- next-dir/src/pages/index.tsx
+++ next-dir/src/pages/index.tsx
@@ -3,13 +3,14 @@ import Head from "next/head";
 // Works
 // import svg1 from "../10.svg?url";
 // import svg2 from "../../../components/10.svg?url";
+// import svgWithAlias from "@components/10.svg?url";
+
 import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [svg, setSvg] = useState(undefined);
 
-  const fetchData = useCallback(
-    async () => {
+  const fetchData = useCallback(async () => {
     console.log("fetchData");
 
     // Works:
@@ -27,16 +28,25 @@ export default function Home() {
     // const svgExports = await import(`../../../components/${file}?url`);
 
     // Works:
-    const file = Math.sqrt(100) + '.svg';
-    const svgExports = await import(`../../../components/${file}?url`);
+    // const svgExports = await import(
+    //   `../../../components/${Math.sqrt(100)}.svg?url`
+    // );
+    // console.log("svgExports", svgExports.default);
 
+    // Works:
+    // const svgExports = await import(`@components/${Math.sqrt(100)}.svg?url`);
+    // console.log("svgExports", svgExports.default);
+
+    // Does not work:
+    const pathWithAlias = `@components/${Math.sqrt(100)}.svg?url`;
+    const svgExports = await import(pathWithAlias);
     console.log("svgExports", svgExports.default);
 
     setSvg(svgExports.default);
   }, []);
 
   useEffect(() => {
-    console.log('useEffect');
+    console.log("useEffect");
     fetchData();
   }, []);
 
@@ -45,11 +55,11 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Hello</title>
+        <title>Next 14 test</title>
       </Head>
       <main>
         <p>svg: {svg}</p>
-        <img src={svg} />
+        {/* <img src={svg} /> */}
       </main>
     </>
   );
